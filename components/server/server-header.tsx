449b5bdd4c08672aced534a2f5b82720b1b83e40
itsnothuy <huytrngqu@gmail.com>
2024-10-17 05:21:02 -0400

    Implement Server Sidebar

---
+++ components/server/server-header.tsx
@@ -0,0 +1,102 @@
+"use client";
+
+
+import { ServerWithMembersWitProfiles } from "@/types";
+import { MemberRole } from "@prisma/client";
+import { DropdownMenu, DropdownMenuItem } from "@radix-ui/react-dropdown-menu";
+import { DropdownMenuContent, DropdownMenuSeparator, DropdownMenuTrigger } from "../ui/dropdown-menu";
+import { ChevronDown, LogOut, PlusCircle, Settings, Trash, User, UserPlus } from "lucide-react";
+interface ServerHeaderProps {
+    server: ServerWithMembersWitProfiles
+    role?: MemberRole;
+};
+
+export const ServerHeader = ({
+    server,
+    role
+}: ServerHeaderProps) => {
+    const isAdmin = role === MemberRole.ADMIN;
+    const isModerator = isAdmin || role === MemberRole.MODERATOR;
+     
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger
+                className="focus:outline-none"
+                asChild
+            >
+                <button 
+                    className="w-full text-md font-semibold px-3 flex
+                    items-center h-12 border-neutral-200
+                    dark:border-neutral-800 border-b-2 hover:bg-zinc-700/10 
+                    dark:hover:bg-zinc-700/50 transition"
+                >
+                    {server.name} 
+                    <ChevronDown className="h-5 w-5 ml-auto" />
+                </button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent
+                className="w-56 text-xs font-medium text-black
+                dark:text-neutral-400 space-y-[2px]"
+            >
+                {isModerator && (
+                    <DropdownMenuItem 
+                        className="text-indigo-600 dark:text-indigo-400
+                        px-3 py-2 text-sm cursor-pointer flex items-center" 
+                    >
+                        Invite People
+                        <UserPlus className="h-4 w-4 ml-auto "/>
+                    </DropdownMenuItem>
+                )}
+
+                {isAdmin && (
+                    <DropdownMenuItem 
+                        className="px-3 py-2 text-sm cursor-pointer flex items-center" 
+                    >
+                        Server Settings
+                        <Settings className="h-4 w-4 ml-auto"/>
+                    </DropdownMenuItem>
+                )}
+
+                {isAdmin && (
+                    <DropdownMenuItem 
+                        className="px-3 py-2 text-sm cursor-pointer flex items-center" 
+                    >
+                        Manage Members
+                        <User className="h-4 w-4 ml-auto"/>
+                    </DropdownMenuItem>
+                )}
+
+                {isModerator && (
+                    <DropdownMenuItem 
+                        className="px-3 py-2 text-sm cursor-pointer flex items-center" 
+                    >
+                        Create Channel
+                        <PlusCircle className="h-4 w-4 ml-auto"/>
+                    </DropdownMenuItem>
+                )}
+
+                {isModerator && (
+                    <DropdownMenuSeparator />
+                )}
+
+                {isAdmin && (
+                    <DropdownMenuItem 
+                        className="text-rose-500 px-3 py-2 text-sm cursor-pointer flex items-center" 
+                    >
+                        Delete Server
+                        <Trash className="h-4 w-4 ml-auto"/>
+                    </DropdownMenuItem>
+                )}
+
+                {!isAdmin && (
+                    <DropdownMenuItem 
+                        className="text-rose-500 px-3 py-2 text-sm cursor-pointer flex items-center" 
+                    >
+                        Leave Server
+                        <LogOut className="h-4 w-4 ml-auto"/>
+                    </DropdownMenuItem>
+                )}
+            </DropdownMenuContent>
+        </DropdownMenu>
+    );
+}
