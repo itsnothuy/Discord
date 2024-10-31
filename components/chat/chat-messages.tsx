ae821908c4015497cf94a085313bcbfd38a2d608
itsnothuy <huytrngqu@gmail.com>
2024-10-30 23:10:49 -0400

    Implement message chat box and chat editing/deleting

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -6,6 +6,11 @@ import { useChatQuery } from "@/hooks/use-chat-query";
 import { Loader2, ServerCrash } from "lucide-react";
 import { Member, Message, Profile } from "@prisma/client";
 import { Fragment } from "react";
+import { ChatItem } from "./chat-item";
+import { format } from "date-fns";
+
+
+const DATE_FORMAT = "d MMM yyyy, HH:mm";
 
 type MessagesWithMemberWithProfile = Message & {
     member: Member & {
@@ -79,9 +84,19 @@ export const ChatMessages = ({
                     {data?.pages?.map((group, i) => (
                         <Fragment key={i} >
                             {group.items.map((message: MessagesWithMemberWithProfile) => (
-                                <div key={message.id}>
-                                    {message.content}
-                                </div>
+                                <ChatItem 
+                                    key={message.id}
+                                    id={message.id}
+                                    currentMember={member}
+                                    member={message.member}
+                                    content={message.content}
+                                    fileUrl={message.fileUrl}
+                                    deleted={message.deleted}
+                                    timestamp={format(new Date(message.createdAt), DATE_FORMAT)}
+                                    isUpdated={message.updatedAt !== message.createdAt}
+                                    socketUrl={socketUrl}
+                                    socketQuery={socketQuery}
+                                />
                             ))}
                         </Fragment>
                     ))}
--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -4,6 +4,7 @@ import { FileIcon, X } from "lucide-react";
 import { useState } from "react";
 import { UploadDropzone } from "@/lib/uploadthing";
 import "@uploadthing/react/styles.css";
+import Image from "next/image";
 
 interface FileUploadProps {
     onChange: (url?: string) => void;
@@ -55,6 +56,20 @@ export const FileUpload = ({
             </div>
         );
     }
+    if (value && fileType !== "application/pdf") {
+        return (
+          <div className="relative h-20 w-20">
+            <Image fill src={value} alt="Upload" className="rounded-full" />
+            <button
+              onClick={() => onChange("")}
+              className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm"
+              type="button"
+            >
+              <X className="h-4 w-4" />
+            </button>
+          </div>
+        );
+      }
 
     // Default UploadDropzone if no file is uploaded yet
     return (
